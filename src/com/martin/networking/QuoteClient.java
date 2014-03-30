93e8763089b8fc749fbcc1eaae2cc61ba64f66a2
Martin Disch <martindisch@gmail.com>
2014-03-30 21:09:16 +0200

    Cleanup

--- src/com/martin/networking/QuoteClient.java
+++ src/com/martin/networking/QuoteClient.java
@@ -31,9 +31,10 @@ package com.martin.networking;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */ 
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class QuoteClient {
     public static void main(String[] args) throws IOException {
